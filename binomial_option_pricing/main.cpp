8cde5dad08711c0c1fcde0e3c66aa2d7179cdfe3
Hadar Greinsmark <hadarg@kth.se>
2017-04-13 15:02:41 +0200

    Fix exercise calc bug

--- binomial_option_pricing/main.cpp
+++ binomial_option_pricing/main.cpp
@@ -15,27 +15,29 @@ double binomial_american_put(double stock_price,
     double down_factor = 1 / up_factor;
     double R = exp((risk_free_rate - dividend_yield) * dt);
     double up_prob = (R - down_factor) / (up_factor - down_factor);
-    double *put = new double[num_steps+1];
+     double *tree = new double[num_steps+1];
 
     // Initialize end of tree at expire time
     for (int step = 0; step <= num_steps; ++step) {
-        put[step] = max(strike_price - stock_price * pow(up_factor, 2 * step - num_steps), .0);
+        // Option value when exercising the option
+        double exercise = strike_price - stock_price * pow(up_factor, 2 * step - num_steps);
+        tree[step] = max(exercise, .0);
     }
 
-    for (int step = num_steps; step >= 0; --step) {
-        for (int branch = 0; branch < step; ++branch) {
-            double binomial = up_prob * put[branch + 1] + (1 - up_prob) * put[branch];
-            double exercise = strike_price - stock_price * pow(up_factor, 2 * step - num_steps);
-            put[branch] = max(binomial, exercise);
+    for (int step = num_steps-1; step >= 0; --step) {
+        for (int branch = 0; branch <= step; ++branch) {
+            double binomial = 1/R * (up_prob * tree[branch + 1] + (1 - up_prob) * tree[branch]);
+            double exercise = strike_price - stock_price * pow(up_factor, (double) 2 * branch - step);
+            tree[branch] = max(binomial, exercise);
         }
     }
 
-    delete[] put;
-
-    return put[0];
+    double price = tree[0];
+    delete[] tree;
+    return price;
 }
 int main() {
-    cout << binomial_american_put(20, 25, .5, 1, 2, 0.06, 0) << endl;
+    cout << binomial_american_put(20, 25, .5, 1, 20, 0.06, 0) << endl;
 
 
     return 0;
