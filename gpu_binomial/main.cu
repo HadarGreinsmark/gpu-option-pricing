a3cad283a986193bbf318a20473a098d3c8fe44d
Hadar Greinsmark <hadarg@kth.se>
2017-04-17 14:52:51 +0200

    Add benchmark

--- gpu_binomial/main.cu
+++ gpu_binomial/main.cu
@@ -35,7 +35,7 @@ __global__ void tree_reduction(
 }
 
 
-double binomial_american_put(double stock_price,
+double gpu1_binomial_american_put(double stock_price,
                              double strike_price,
                              double expire,
                              double volat,
@@ -69,20 +69,83 @@ double binomial_american_put(double stock_price,
     delete[] host_tree;
     return price;
 }
-int main() {
+
+
+
+// Build tree on GPU directly
+
+__global__ void tree_builder(
+		double* tree,
+		double stock_price,
+		double strike_price,
+		int num_steps,
+		double R,
+		double up_factor,
+		double up_prob) {
+
+    // Initialize end of host_tree at expire time
+	int branch = threadIdx.x;
+    if (branch <= num_steps) {
+        // Option value when exercising the option
+        double exercise = strike_price - stock_price * pow(up_factor, 2 * branch - num_steps);
+        tree[branch] = max(exercise, .0);
+    }
+
+    for (int step = num_steps-1; step >= 0; --step) {
+    	int branch = threadIdx.x;
+    	if (branch <= step) {
+            double binomial = 1/R * (up_prob * tree[branch + 1] + (1 - up_prob) * tree[branch]);
+            double exercise = strike_price - stock_price * pow(up_factor, 2 * branch - step);
+            tree[branch] = max(binomial, exercise);
+    	}
+    	__syncthreads();
+    }
+}
+
+
+double gpu2_binomial_american_put(double stock_price,
+                             double strike_price,
+                             double expire,
+                             double volat,
+                             int num_steps,
+                             double risk_free_rate) {
+    double dt = expire / num_steps;
+    double up_factor = exp(volat * sqrt(dt));
+    double down_factor = 1 / up_factor;
+    double R = exp((risk_free_rate) * dt);
+    double up_prob = (R - down_factor) / (up_factor - down_factor);
+    double* dev_tree;
+    double price;
+
+    check_err(cudaMalloc((void**) &dev_tree, (num_steps+1)*sizeof(double)));
+
+    tree_builder<<<1, num_steps>>>(dev_tree, stock_price, strike_price, num_steps, R, up_factor, up_prob);
+
+    check_err(cudaMemcpy(&price, &dev_tree[0], sizeof(double), cudaMemcpyDeviceToHost));
+    cudaFree(dev_tree);
+
+    return price;
+}
+
+
+
+
+void benchmark_gpu(int (*to_invoke)()) {
 	cudaEvent_t start, end;
 	check_err(cudaEventCreate(&start));
 	check_err(cudaEventCreate(&end));
 
-    // Warmup
+	printf("Function returns: %d\n", to_invoke());
+
+	printf("Warm up");
     for (int i = 0; i < 100; ++i) {
-        binomial_american_put(20, 25, .5, 1, 200, 0.06);
+    	to_invoke();
     }
 
+    printf("Start test\n");
     check_err(cudaEventRecord(start, 0));
-
     for (int i = 0; i < 1000; ++i) {
-        binomial_american_put(20, 25, .5, 1, 200, 0.06);
+        to_invoke();
     }
 
     check_err(cudaEventRecord(end, 0));
@@ -93,7 +156,20 @@ int main() {
     check_err(cudaEventDestroy(start));
     check_err(cudaEventDestroy(end));
 
-    printf("Time: %d ms\n", duration);
+    printf("Took %d ms\n", int(duration + 0.5));
+}
+
+int gpu1() {
+	return gpu1_binomial_american_put(20, 25, .5, 1, 200, 0.06);
+}
+
+int gpu2() {
+	return gpu2_binomial_american_put(20, 25, .5, 1, 200, 0.06);
+}
+
+int main() {
+	benchmark_gpu(gpu1);
+	benchmark_gpu(gpu2);
 
     return 0;
 }
